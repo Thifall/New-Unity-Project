532c4689dead3e823501ee74fdbd64ed0c125640
Thifall <migosh@o2.pl>
2018-06-03 18:39:06 +0200

    Chapter 8 Prototypes
    Done prototypes of place and enemies inside the "boss" encounter

--- Assets/Scripts/CameraArm.cs
+++ Assets/Scripts/CameraArm.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class CameraArm : MonoBehaviour
 {
     private GameObject _player;
-    private bool _backPedaling;
 
     private void Start()
     {
@@ -15,43 +14,11 @@ public class CameraArm : MonoBehaviour
 
     private void Update()
     {
-        _backPedaling = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
     }
 
     void LateUpdate()
     {
-        if (!_backPedaling)
-        {
-            RotateCamera();
-        }
-        AdjustPosition();
-    }
-
-    private void RotateCamera()
-    {
-        if (!Input.GetMouseButton(1))
-        {
-            FollowPlayerRotation();
-        }
-        else
-        {
-            RotateCameraBasedOnMouseRotation();
-        }
-    }
-
-    private void RotateCameraBasedOnMouseRotation()
-    {
-        var rotation = Input.GetAxis("Mouse X");
-        this.transform.Rotate(0, rotation * 5, 0f);
-    }
-
-    private void FollowPlayerRotation()
-    {
-        this.transform.rotation = _player.transform.rotation;
-    }
-
-    private void AdjustPosition()
-    {
         this.transform.position = _player.transform.position;
     }
+    
 }
