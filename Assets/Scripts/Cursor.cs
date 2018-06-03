12a6213ea5a9f11a1e7f2ce1b3ef203393d1082e
Thifall <migosh@o2.pl>
2018-06-03 22:21:54 +0200

    chapter 9 and 10
    RayCasting excercises

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+    CameraRaycaster _rayCaster;
+	// Use this for initialization
+	void Start () {
+		_rayCaster = GetComponent<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            print(_rayCaster.layerHit);
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/Enums.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets.Scripts.Utility
+{
+    public enum Layer
+    {
+        Walkable = 9,
+        Enemy = 10,
+        RaycastEndStop = -1
+    }
+}
